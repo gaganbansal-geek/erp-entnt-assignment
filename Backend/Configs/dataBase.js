d643afeebfe628858063d6264ed42f1666d8b253
gaganbansal21 <gagan@attryb.com>
2024-03-15 11:00:10 +0530

    new

--- Backend/Configs/dataBase.js
+++ Backend/Configs/dataBase.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const dataBaseConnection = mongoose.connect(
+  //connecting friend mongodb account due to having problem in my cluster
   "mongodb+srv://hanumat:hanumat@cluster0.aqmtmxk.mongodb.net/ecommerce?retryWrites=true&w=majority&appName=Cluster0"
 );
 
